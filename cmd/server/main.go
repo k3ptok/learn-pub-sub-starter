c10c55be46309c6bfd4d0c870c1dfcbd68e8e2dc
High-Quality-Dog-Water <sandrock26173@hotmail.com>
2026-07-04 09:45:43 -0600

    first commit. Added connection path to rabbitMQ, and added ctrl+c intercept

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"os/signal"
+	"os"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connPath := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection Successful!")
+
+	//wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting Down...")
 }
